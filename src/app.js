0c60e732ef3d547d749d6503d32b5983dc5a33d1
Renata Ernestina <renatarene.silva573@gmail.com>
2020-07-05 15:00:08 -0300

    Teste de verificação do arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import { accountsRouter } from './routers/accountsRouter';
 
 const app = express();
 
 app.use(express.json());
+app.use(accountsRouter);
 
 app.listen(3000, () => {
   console.log('API iniciada');
